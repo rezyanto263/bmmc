e158e7a18909900d01be79f6a486297d81281484
Sagita Narayana <sagita.ketut213@gmail.com>
2024-11-19 09:47:19 +0800

    Partials UserPage

--- application/controllers/user/User.php
+++ application/controllers/user/User.php
@@ -1,10 +1,40 @@
-<?php
+<?php 
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
 
-    public function profile() {
-        // Memuat view tanpa data tambahan
-        $this->load->view('user/user_profile');
+    public function __construct()
+    {
+        parent::__construct();
+        if ($this->session->userdata('adminRole') != ('admin' || 'user')) {
+            redirect('dashboard');
+        }
+
     }
+    
+
+    public function index()
+    {
+        $datas = array(
+            'title' => 'BIM | User',
+            'subtitle' => 'User',
+            'contentType' => 'user'
+        );
+
+        $partials = array(
+            'head' => 'partials/head',
+            'navbar' => 'partials/user/navbar',
+            'content' => 'user/userPage',
+            'script' => 'partials/script'
+        );
+
+        $this->load->vars($datas);
+        $this->load->view('master', $partials);
+    }
+
 }
+
+/* End of file Company.php */
+
+?>
---
+++ application/views/partials/user/navbar.php
@@ -0,0 +1,8 @@
+<nav>
+    <div class="container d-flex justify-content-between">
+        <i><img src="<?= base_url('assets/images/logo.png')?>" alt="" style="width: 50px; height: 50px;" class="m-2"></i>
+        <a href="#" class="d-flex align-items-center gap-1 m-2"   data-bs-toggle="tooltip"data-bs-placement="right" data-bs-title="Profile">
+            <i class="las la-user-circle fs-4" ></i><span>Profile</span>
+        </a>
+    </div>
+</nav>
